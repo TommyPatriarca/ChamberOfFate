776c3065a190decf244ad6b78c0dacc5d5af7752
Mikicoma06 <michelecoma06@gmail.com>
2025-01-24 08:39:22 +0100

    javaDoc player

--- src/main/java/com/cof/player/Player.java
+++ src/main/java/com/cof/player/Player.java
@@ -5,17 +5,27 @@ import com.cof.game.Card;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * La classe del giocatore
+ */
 public class Player {
     private String name;
     private int health = 5; // Correzione di "healt" in "health"
     private List<Card> hand; // Lista delle carte del giocatore
 
+    /**
+     * Il costruttore del giocaotre
+     * @param name il nome del giocatore
+     */
     public Player(String name) {
         this.name = name;
         this.hand = new ArrayList<>(); // Inizializza la mano del giocatore
     }
 
-    // Rimuove punti vita al giocatore
+    /**
+     * Rimuove punti vita al giocatore
+     * @param amount il danno
+     */
     public void removeLife(int amount) {
         if (health > amount) {
             health -= amount;
@@ -25,7 +35,10 @@ public class Player {
         }
     }
 
-    // Aggiunge punti vita al giocatore (massimo 5)
+    /**
+     * Aggiunge punti vita al giocatore (massimo 5)
+     * @param amount il danno
+     */
     public void addLife(int amount) {
         if (5 - health > amount) {
             health += amount;
@@ -34,7 +47,10 @@ public class Player {
         }
     }
 
-    // Pesca una carta e la aggiunge alla mano
+    /**
+     * Pesca una carta e la aggiunge alla mano
+     * @param card la carta
+     */
     public void drawCard(Card card) {
         if (card != null) {
             hand.add(card);
@@ -43,7 +59,10 @@ public class Player {
         }
     }
 
-    // Calcola il valore delle carte nella mano (come nel Black Jack)
+    /**
+     * Calcola il valore delle carte nella mano (come nel Black Jack)
+     * @return il totale delle carte in mano
+     */
     public int calculateHandValue() {
         int totalValue = 0;
         int aceCount = 0;
@@ -77,22 +96,35 @@ public class Player {
         return totalValue;
     }
 
-    // Restituisce il nome del giocatore
+    /**
+     * Restituisce il nome del giocatore
+     * @return il nome del giocatore
+     */
     public String getName() {
         return name;
     }
 
-    // Restituisce i punti vita del giocatore
+    /**
+     * Restituisce i punti vita del giocatore
+     * @return la vita del giocatore
+     */
+    //
     public int getHealth() {
         return health;
     }
 
-    // Restituisce le carte nella mano del giocatore
+    /**
+     * Restituisce le carte nella mano del giocatore
+     * @return le carte in mano del giocatore
+     */
     public List<Card> getHand() {
         return hand;
     }
 
-    // Stampa lo stato del giocatore
+    /**
+     * Stampa lo stato del giocatore
+     * @return stato del giocatore
+     */
     @Override
     public String toString() {
         return "Player{" +
